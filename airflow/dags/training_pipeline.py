277f3c27cde058e95294c6c5f90709d582c1b276
Urvil-Dhanani <urvild@gmail.com>
2024-05-31 12:04:30 +0530

    airflow on half way

--- airflow/dags/training_pipeline.py
+++ airflow/dags/training_pipeline.py
@@ -7,5 +7,20 @@ from src.pipeline.training_pipeline import TrainingPipeline
 
 train_pipeline = TrainingPipeline()
 
-with DAG()
+with DAG(
+    "pricepredictor_training_pipeline",
+    default_args={"retries": 2},
+    description="my first airflow DAG",
+    schedule="@weekly",
+    start_date=pendulam.datetime(2024, 5, 30),
+    catchup=False,
+    tags=["machine_learning", "regression", "price_prediction"]) as dag:
+    
+    dag.doc_md = __doc__
 
+    def data_ingestion(**kwargs):
+        ti = kwargs["ti"]
+        train_path, test_path = TrainingPipeline.start_data_ingestion()
+        ti.xcom_push("data_ingestion_artifacts", {"train_path":train_path, "test_path":test_path})
+
+    
